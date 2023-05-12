14717d4d19171624bc7782a7ac23e50d05c9516a
Nicolas.pino <nicolaspino818@gmail.com>
2023-05-12 00:14:17 -0400

    askjdla

--- src/app/servicios/api-music-pro/api-music-pro.service.ts
+++ src/app/servicios/api-music-pro/api-music-pro.service.ts
@@ -25,5 +25,19 @@ export class ApiMusicProService {
     return this.http.get(`${environment.musicproBaseRoute}data/`,{headers: this.headers});
   }
 
+
+  getCategorias():Observable<any>{
+    return this.http.get(`${environment.musicproBaseRoute}data/`,{headers: this.headers});
+  }
+
+  getProductosByCategoria(categoria:string):Observable<any>{
+    return this.http.get(`${environment.musicproBaseRoute}products/${categoria}`,{headers: this.headers});
+
+  }
+  login({user,passw}):Observable<any>{
+    return this.http.post(`${environment.musicproBaseRoute}login/`,{user,passw},{headers: this.headers});
+
+  }
+
   
 }
